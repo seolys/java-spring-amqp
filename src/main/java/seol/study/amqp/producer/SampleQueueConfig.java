b67baa7b3c28e9b265728102d009437adec4a757
설연수 <seolnavy@naver.com>
2021-10-10 13:14:33 +0900

    topic 옵션 조정

--- src/main/java/seol/study/amqp/producer/SampleQueueConfig.java
+++ src/main/java/seol/study/amqp/producer/SampleQueueConfig.java
@@ -17,7 +17,7 @@ public class SampleQueueConfig {
 	public static final String SAMPLE_QUEUE_NAME = "sample.topic.queue";
 	public static final String SAMPLE_ROUTING_KEY = "sample.topic.queue";
 	public static final String SAMPLE_DURABLE = "true";
-	public static final String SAMPLE_AUTO_DELETE = "true";
+	public static final String SAMPLE_AUTO_DELETE = "false";
 
 	public static final String SAMPLE_EXCHANGE_DLX_NAME = "sample.topic.exchange.dlx";
 	public static final String SAMPLE_QUEUE_DLX_NAME = "sample.topic.queue.dlx";
